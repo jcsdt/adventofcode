5749364693d52de5e35da7d41a64f1bf5064cdfd
Jean Casademont <jean.casademont@gmail.com>
2018-12-03 15:44:11 +0100

    Day 2 - part 1

---
+++ src/bin/day2.rs
@@ -0,0 +1,34 @@
+use std::collections::{HashMap, HashSet};
+use std::io::BufRead;
+
+use aoc::runner::{run, Result};
+
+fn count_letters(string: &str) -> HashSet<i32> {
+    let mut counts: HashMap<char, i32> = HashMap::new();
+
+    for l in string.chars() {
+        let counter = counts.entry(l).or_insert(0);
+        *counter += 1;
+    }
+
+    counts.drain().map(|(_, v)| v).collect::<HashSet<i32>>()
+}
+
+fn select_two_and_three(counts: HashSet<i32>) -> (i32, i32) {
+    let two = if counts.contains(&2) { 1 } else { 0 };
+    let three = if counts.contains(&3) { 1 } else { 0 };
+
+    (two, three)
+}
+
+fn main() -> Result<()> {
+    run(|reader| {
+        let (x, y) = reader
+            .lines()
+            .map(|s| count_letters(&s.unwrap()))
+            .map(|letters| select_two_and_three(letters))
+            .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
+
+        Ok(x * y)
+    })
+}
